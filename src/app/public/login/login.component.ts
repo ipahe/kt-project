4a77b425359469add250a77d92683774ba8811d4
I.Parra <i.parra@cellfusio.com>
2017-12-26 12:48:55 -0600

    Binding form data

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit{
 
   public isAuthenticated = false;
+  public cadena = "Hola soy el Login components";
+  public id = Math.random().toString(36).substring(4);
+  public user: any = {};
 
   constructor() { }
 
   ngOnInit() {
+    let isAuthenticated = true;
     console.log("isAuthenticated", this.isAuthenticated);
+    
   }
 
-  onSubmit(event){
-
+  onSubmit(event: Event){
+    event.preventDefault();
+    console.log(this.user);
   }
+  
 
 }
